697fb8227b51c9219862339794648655f6a24464
Christine Ngoc Tran <chtra2129@oslomet.no>
2024-04-19 10:02:45 +0200

    Commit alt

---
+++ src/main/java/oslometwebpro/oblig3/BillettController.java
@@ -0,0 +1,34 @@
+package oslometwebpro.oblig3;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class BillettController {
+
+    @Autowired
+    private BillettRepository rep;
+
+    @PostMapping("/lagre")
+    public void lagreBillett(Billett billett) {
+        rep.lagreBillett(billett);
+    }
+
+    @GetMapping("/hentAlle")
+    public List<Billett> hentAlle(){
+        return rep.hentAlleBilletter();
+    }
+
+    @GetMapping("/slettAlle")
+    public void slettAlle() {
+        rep.slettAlleBilletter();
+    }
+
+}
+
+
