88b2d5adcbe1d5c4c2a7deb3e5b84821a0301d79
Christine Ngoc Tran <chtra2129@oslomet.no>
2024-04-21 19:44:53 +0200

    Nye valideringsinputer + nye kommentarer

--- src/main/java/oslometwebpro/oblig3/BillettController.java
+++ src/main/java/oslometwebpro/oblig3/BillettController.java
@@ -1,6 +1,5 @@
 package oslometwebpro.oblig3;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
